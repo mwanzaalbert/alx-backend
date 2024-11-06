55f432438fe4b2f57756105b01211e8b4096e6d1
mwanzaalbert <user.email>
2024-11-06 09:58:08 +0300

    missing assignement operator added

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -27,7 +27,7 @@ class Config:
 
 
 # Mock user database
-users{
+users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
